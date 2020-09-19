f53b3be87795ba82bcbd843fd9b7e08711ae780b
oxilgk <vazkats@gmail.com>
2020-09-19 18:03:31 +0300

    Implementing the opcodes

--- c8.c
+++ c8.c
@@ -33,7 +33,7 @@ const unsigned char fontset[80] = {
 
 typedef struct {
 	unsigned char mem[4096];
-	unsigned char reg[16];
+	unsigned char V[16];
 	unsigned char gx[64*32];
 	unsigned short op;
 	unsigned short i;
@@ -50,10 +50,10 @@ CHIP8 new_chip8() {
 
 	// Initializing the system
 	CHIP8 c;
-	c.sp = 0x200;
+	c.pc = 0x200;
 
 	// Opening the game to read
-	FILE *f = fopen("breakout.ch8", "r");
+	FILE *f = fopen("s.ch8", "r");
 	if (f == NULL) {
 		fprintf(stderr, "NOT FOUND\n");
 		exit(-1);
@@ -72,13 +72,60 @@ CHIP8 new_chip8() {
 		exit(-1);
 	}
 
+	// Filling the struct with game's data
 	for (int i = 0; i < flsize; i++) 
 		c.mem[i + 512] = buffer[i];
+
+	// Copying the font set to the struct
+	memcpy(&c.font, &fontset, 80);
 
 	return c;
 }
 
 void emulate_cycle(CHIP8 *c) {
+	uint16_t op = c->mem[c->pc] << 8 | c->mem[c->pc + 1];
+	switch (op & 0xF000) {
+	case 0x0000: // Clears the screen
+		SDL_RenderClear(r);
+		break;
+	case 0x000F:
+		c->pc = c->stack[c->sp];
+		c->sp--;
+		break;
+	case 0x1000:
+		c->pc = op & 0x0FFF;
+		break;
+	case 0x2000:
+		c->stack[c->sp] = c->pc;
+		c->sp++;
+		c->pc = op & 0x0FFF;
+		break;
+	case 0x3000:
+		if (c->V[(op & 0x0F00) >> 8] == (op & 0x00FF)) c->pc += 4;
+		break;
+	case 0x4000:
+		if (c->V[(op & 0x0F000) >> 8] != (op & 0x00FF)) c->pc += 4;
+		break;
+	case 0x5000:
+		if (c->V[(op & 0x0F00) >> 8] == c->V[(op & 0x00F0) >> 4]) c->pc += 4;
+		break;
+	case 0x6000:
+		c->V[(op & 0x0F00) >> 8] = (unsigned char)op; //(op & 0x00FF) >> 8;
+		c->pc += 2;
+		break;
+	case 0x7000:
+		c->V[(op & 0x0F00) >> 8] += (unsigned char)op;
+		c->pc += 2;
+		break;
+	case 0x8000:
+		switch (op & 0x000F) {
+		case 0x0000:
+			c->V[(op & 0x0F00) >> 8] = c->[(op & 0x00F) >> 4]; 
+			c->pc += 2;
+			break;
+	}
+
+
 
 }
 
@@ -105,6 +152,8 @@ int sdl_setup() {
 		return EXIT_FAILURE;
 	}
 
+	return 0;
+
 
 }
 
@@ -116,22 +165,34 @@ void event() {
 		case SDL_QUIT:
 			quit = true;
 			break;
+
+		case SDL_KEYDOWN:
+			switch (e.key.keysym.sym) {
+			case SDLK_a:
+				SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
+				break;
+			case SDLK_b:
+				SDL_SetRenderDrawColor(r, 0x44, 0x44, 0x44, 0xff);
+				break;
+			}
 		}
 	}
 }
-
 int
 main()
 {
 	sdl_setup();
-	CHIP8 a = new_chip8();
+	CHIP8 b = new_chip8();
 
-	SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
+	emulate_cycle(&b);
+
+	SDL_SetRenderDrawColor(r, 0x33, 0x33, 0x33, 0xff);
 	while(!quit) {
 		event();
 		SDL_RenderClear(r);
 		SDL_RenderPresent(r);
 	}
+
 
 	SDL_DestroyRenderer(r);
 	SDL_DestroyWindow(w);
