df948a4bff4bbea3aff2375b6d7840608e16d355
oxilgk <vazkats@gmail.com>
2020-09-21 23:41:24 +0300

    Implemented all ops

--- c8.c
+++ c8.c
@@ -4,39 +4,23 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+#define BUFF_LEN 100000
 // Globals
-
 SDL_Window   *w;
 SDL_Renderer *r;
-bool quit    = false;
+bool running  = true;
+bool draw_f   = false;
 
+const unsigned char fontset[80] = { 0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10, 0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0, 0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0, 0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80  };
 
-#define BUFF_LEN 100000
-const unsigned char fontset[80] = {
-			0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-			0x20, 0x60, 0x20, 0x20, 0x70, // 1
-			0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-			0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-			0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-			0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-			0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-			0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-			0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-			0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-			0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-			0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-			0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-			0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-			0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-			0xF0, 0x80, 0xF0, 0x80, 0x80  // F
-};
+// Structs
 
 typedef struct {
 	unsigned char mem[4096];
 	unsigned char V[16];
-	unsigned char gx[64*32];
+	unsigned char gfx[64*32];
 	unsigned short op;
-	unsigned short i;
+	unsigned short I;
 	unsigned short pc;
 	unsigned char dt;
 	unsigned char st;
@@ -120,11 +104,147 @@ void emulate_cycle(CHIP8 *c) {
 	case 0x8000:
 		switch (op & 0x000F) {
 		case 0x0000:
-			c->V[(op & 0x0F00) >> 8] = c->[(op & 0x00F) >> 4]; 
+			c->V[(op & 0x0F00) >> 8] = c->V[(op & 0x00F) >> 4]; 
 			c->pc += 2;
 			break;
-	}
+		case 0x0001:
+			c->V[(op & 0x0F00) >> 8] = \
+				c->V[(op & 0x0F00) >> 8] | c->V[(op & 0x00F0) >> 4];
+			c->pc += 2;
+			break;
+		case 0x0002:
+			c->V[(op & 0x0F00) >> 8] = \
+				c->V[(op & 0x0F00) >> 8] & c->V[(op & 0x00F0) >> 4];
+			c->pc += 2;
+			break;
+		case 0x0003:
+			c->V[(op & 0x0F00) >> 8] = \
+			   c->V[(op & 0x0F00) >> 8] ^ c->V[(op & 0x00F0) >> 4];
+			c->pc += 2;
+			break;
+		case 0x0004:
+			c->V[0xF] =	c->V[(op & 0x00F0) >> 4] > 0xFF - c->V[(op & 0x0F00) >> 8] ? 1 :  0;
+			c->V[(op & 0x0F00) >> 8] += c->V[(op & 0x00F0) >> 4];
+			c->pc += 2;
+			break;
+		case 0x0005:
+			c->V[0xF] =	(c->V[(op & 0x00F0) >> 4] > c->V[(op & 0x0F00) >> 8]) ? 0 :  1;
+			c->V[(op & 0x0F00) >> 8] -= c->V[(op & 0x00F0) >> 4];
+			c->pc += 2;
+			break;
 
+		case 0x0006:
+			c->V[0xF] = c->V[(op & 0x0F00) >> 8] & 0x01 ? 1 : 0;
+			c->V[(op & 0x0F00) >> 8] >>= 1;
+			c->pc += 2;
+			break;
+		case 0x0007:
+			c->V[0xF] =	(c->V[(op & 0x0F00) >> 8] > c->V[(op & 0x00F0) >> 4]) ? 0 :  1;
+			c->V[(op & 0x00F0) >> 4] -= c->V[(op & 0x0F00) >> 8];
+			c->pc += 2;
+			break;
+		case 0x000E:
+			c->V[0xF] = c->V[(op & 0x0F00) >> 8] & 0x10 ? 1 : 0;
+			c->V[(op & 0x0F00) >> 8] <<= 1;
+			c->pc += 2;
+			break;
+		}
+	case 0x9000:
+		if (c->V[(op & 0x0F00) >> 8] != c->V[(op & 0x00F0) >> 4]) {
+			c->pc +=2;
+		}
+		c->pc += 2;
+		break;
+	case 0xA000:
+		c->I  = op & 0x0FFF;
+		c->pc += 2;
+		break;
+
+	case 0xB000:
+		c->pc = c->V[0x0] + (op & 0x0FFF);
+		break;
+
+	case 0xC000:
+		c->V[(op & 0x0F00) >> 8] = \
+			(rand() % 255) & (op & 0xFF);
+		c->pc += 2;
+		break;
+
+	case 0XD000:
+	{
+		unsigned short x = c->V[(op & 0x0F00) >> 8];
+		unsigned short y = c->V[(op & 0x00F0) >> 4];
+		unsigned short h = op & 0x000F;
+		unsigned short p;
+
+		c->V[0xF] = 0;
+		for (int yline = 0; yline < h; yline++) {
+			p = c->mem[c->I + yline];
+			for (int xline = 0; xline < 8; xline++) {
+				if((p & (0x80 >> xline)) != 0) {
+					if(c->gfx[(x + xline + ((y + yline) * 64))] == 1)
+						c->V[0xF] = 1;                                 
+					c->gfx[x + xline + ((y + yline) * 64)] ^= 1;
+				}
+			}
+		}
+		draw_f = true;
+		c->pc += 2;
+	}
+		break;
+
+	case 0xE000:
+		switch (op & 0x00FF) {
+		case 0x009E: c->pc += (c->key[c->V[(op & 0x0F00) >> 8]] != 0) ? 4 : 2; break;
+		case 0x00A1: c->pc += (c->key[c->V[(op & 0x0F00) >> 8]] == 0) ? 4 : 2; break;
+		}
+		break;
+
+	case 0xF000:
+		switch (op & 0x00FF) {
+		case 0x0007: c->V[(op & 0x0F00) >> 8] = c->dt; c->pc += 2;
+			
+		case 0x000A: 
+		{
+			bool keypress = false;
+			for (int i = 0; i < 16; i++) {
+				if (c->key[i] != 0) {
+					c->V[(op &0x0F00) >> 8] = c->key[i];
+					keypress = true;
+				}
+			}
+			if (!keypress) return;
+
+			c->pc += 2;
+		}
+		break;
+
+		case 0x0015: c->dt = c->V[(op & 0x0F00) >> 8]; c->pc += 2; break;
+		case 0x0018: c->st = c->V[(op & 0x0F00) >> 8]; c->pc += 2; break;
+		case 0x001E: c->I += c->V[(op & 0x0F00) >> 8]; c->pc += 2; break;
+		case 0x0029: c->I += c->V[(op & 0x0F00) >> 8]; c->pc += 2; break;
+		case 0x0033: 
+			c->mem[c->I]     = c->V[(op & 0x0F00) >> 8] / 100;
+			c->mem[c->I + 1] = (c->V[(op & 0x0F00) >> 8] / 10) % 10;
+			c->mem[c->I + 2] = (c->V[(op & 0x0F00) >> 8] % 100) % 10;
+			c->pc += 2;
+		break;
+		case 0x0055:
+			for (int i = 0; i < c->V[(op & 0x0F00) >>8]; i++) {
+				c->mem[c->I + i] = c->V[i];
+			}
+			c->pc += 2;
+			break;
+		case 0x0065:
+			for (int i = 0; i < c->V[(op & 0x0F00) >>8]; i++) {
+				c->V[i] = c->mem[c->I + i];
+			}
+			c->pc += 2;
+			break;
+
+		}
+		break;
+	}
 
 
 }
@@ -152,44 +272,105 @@ int sdl_setup() {
 		return EXIT_FAILURE;
 	}
 
+
+	SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
+
 	return 0;
 
 
 }
 
 
-void event() {
+void event(CHIP8 *c) {
 	SDL_Event e;
 	if (SDL_WaitEvent(&e) != 0) {
 		switch (e.type) {
 		case SDL_QUIT:
-			quit = true;
+			running = false;
 			break;
 
 		case SDL_KEYDOWN:
 			switch (e.key.keysym.sym) {
-			case SDLK_a:
-				SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
-				break;
-			case SDLK_b:
-				SDL_SetRenderDrawColor(r, 0x44, 0x44, 0x44, 0xff);
-				break;
+
+			case SDLK_ESCAPE: running = false; break;
+
+			case SDLK_1: 	c->key[0x1] = 1; break; 
+			case SDLK_2:	c->key[0x2] = 1; break;
+			case SDLK_3:	c->key[0x3] = 1; break;
+			case SDLK_4:	c->key[0xC] = 1; break;
+
+			case SDLK_q:	c->key[0x4] = 1; break;
+			case SDLK_w:	c->key[0x5] = 1; break;
+			case SDLK_e:	c->key[0x6] = 1; break;
+			case SDLK_r:	c->key[0xD] = 1; break;
+
+			case SDLK_a:	c->key[0x7] = 1; break;
+			case SDLK_s:	c->key[0x8] = 1; break;
+			case SDLK_d:	c->key[0x9] = 1; break;
+			case SDLK_f:	c->key[0xE] = 1; break;
+
+			case SDLK_z:	c->key[0xA] = 1; break;
+			case SDLK_x:	c->key[0x0] = 1; break;
+			case SDLK_c:	c->key[0xB] = 1; break;
+			case SDLK_v:	c->key[0xF] = 1; break;
+
+			}
+			break;
+		case SDL_KEYUP:
+			switch (e.key.keysym.sym) {
+			case SDLK_1:	c->key[0x1] = 0; break;   
+			case SDLK_2:	c->key[0x2] = 0; break;
+			case SDLK_3:	c->key[0x3] = 0; break;
+			case SDLK_4:	c->key[0xC] = 0; break;
+
+			case SDLK_q:	c->key[0x4] = 0; break;
+			case SDLK_w:	c->key[0x5] = 0; break;
+			case SDLK_e:	c->key[0x6] = 0; break;
+			case SDLK_r:	c->key[0xD] = 0; break;
+
+			case SDLK_a:	c->key[0x7] = 0; break;
+			case SDLK_s:	c->key[0x8] = 0; break;
+			case SDLK_d:	c->key[0x9] = 0; break;
+			case SDLK_f:	c->key[0xE] = 0; break;
+
+			case SDLK_z:	c->key[0xA] = 0; break;
+			case SDLK_x:	c->key[0x0] = 0; break;
+			case SDLK_c:	c->key[0xB] = 0; break;
+			case SDLK_v:	c->key[0xF] = 0; break;
+			break;
 			}
 		}
 	}
 }
+
+void update(CHIP8 *c) {
+	SDL_Rect rect = {0, 0, 8, 0};
+
+	for (int y = 0; y < 32; y++) {
+		for (int x = 0; x < 64; x++) {
+			if (c->gfx[y * 32 + x] != 0) {
+				rect.x = x;
+				rect.y = y;
+				continue;
+			}
+
+		}
+	}
+	return;
+
+
+}
 int
 main()
 {
-	sdl_setup();
 	CHIP8 b = new_chip8();
-
+	srand(time(NULL));
 	emulate_cycle(&b);
 
-	SDL_SetRenderDrawColor(r, 0x33, 0x33, 0x33, 0xff);
-	while(!quit) {
-		event();
-		SDL_RenderClear(r);
+	sdl_setup();
+	while(running) {
+		event(&b);
+		if (draw_f) SDL_RenderClear(r);
 		SDL_RenderPresent(r);
 	}
 
